85c577e02873e902a72e378ca29b973b8570c54c
wstarywbw12 <wstarywbw@gmail.com>
2026-06-19 17:39:31 +0700

    log

--- config/survei.example.php
+++ config/survei.example.php
@@ -2,8 +2,8 @@
 
 return [
     'login_url' => 'https://it.larsdhp.or.id/survei-v2/login/sign',
-    'email'     => 'rsudjombang@jombangkab.go.id',
-    'password'  => 'LARSDHP2022#Survei',
+    'email'     => '',
+    'password'  => '',
 
     // cookie dianggap expired setelah 2 jam
     'cookie_lifetime' => 7200,
